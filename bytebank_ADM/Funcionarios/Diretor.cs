fa427e5ab765fcc6136ec5cf39d79c910a44fdb8
CiCristina <cibellycristinaudi@gmail.com>
2023-03-06 09:58:49 -0300

    Entendendo herança

---
+++ bytebank_ADM/Funcionarios/Diretor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Funcionarios
+{
+    public class Diretor : Funcionario //diretor herda de funcionario
+    {
+        public override double GetBonificacao() //esse metodo é uma redefinição do q foi escrito classe funcion
+        {
+            return this.Salario + base.GetBonificacao(); //base me da acesso aos elementos cs funcionario
+        }
+    }
+}
